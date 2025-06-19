b3e4ed91496013476ac8d52e7b2f406b3eddf9a3
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-19 08:23:01 +0200

    created userGreeting component to learn how exactly works conditional in React.

--- br_code/src/App.jsx
+++ br_code/src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import Student from './components/Student'
+import UserGreeting from './components/UserGreeting'
 
 function App() {
 
 
   return (
     <>
-
+       <UserGreeting isLogged= {true} username='BroCode' />
     </>
   )
 }
---
+++ br_code/src/components/UserGreeting.jsx
@@ -0,0 +1,7 @@
+export default function UserGreeting() {
+    return (
+        <>
+        
+        </>
+    )
+}
