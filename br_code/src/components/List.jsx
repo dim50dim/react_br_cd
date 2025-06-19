1d2197bfab1c8ff0e88e8a8958ec154ba794dd4c
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-19 10:19:53 +0200

    used sort() in a different order.

--- br_code/src/components/List.jsx
+++ br_code/src/components/List.jsx
@@ -1,7 +1,17 @@
 export default function List() {
-    const fruits = ['apple','orange', 'banana','pineapple'];
+    const fruits = [
+        {id:1,name:'apple',calories: 44},
+        {id:2,name: 'orange',calories: 33},
+        {id:3,name: 'banana',calories: 66},
+        {id:4,name: 'pineapple',calories:22},
+    ]
+    // fruits.sort((a,b) =>  b.name.localeCompare(a.name) )
+    // fruits.sort((a,b) => b.calories - a.calories);
+
     
-    const listItems = fruits.map(fruit => <li> {fruit} </li>)
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+                                                            {fruit.name} : &nbsp;
+                                                           {fruit.calories}</li>)
     return (
         <ol>
             {listItems}
