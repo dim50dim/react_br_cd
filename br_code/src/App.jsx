957e2388ecf49ea4cc70a426040f93caacfcd071
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-19 10:25:21 +0200

    used the same method , but through props.

--- br_code/src/App.jsx
+++ br_code/src/App.jsx
@@ -5,11 +5,16 @@ import Student from './components/Student'
 import UserGreeting from './components/UserGreeting'
 
 function App() {
-
+    const fruits = [
+        {id:1,name:'apple',calories: 44},
+        {id:2,name: 'orange',calories: 33},
+        {id:3,name: 'banana',calories: 66},
+        {id:4,name: 'pineapple',calories:22},
+    ]
 
   return (
     <>
-     <List/>
+     <List items={fruits} category='Fruits' />
     </>
   )
 }
--- br_code/src/components/List.jsx
+++ br_code/src/components/List.jsx
@@ -1,15 +1,14 @@
-export default function List() {
-    const fruits = [
-        {id:1,name:'apple',calories: 44},
-        {id:2,name: 'orange',calories: 33},
-        {id:3,name: 'banana',calories: 66},
-        {id:4,name: 'pineapple',calories:22},
-    ]
+export default function List(props) {
+
     // fruits.sort((a,b) =>  b.name.localeCompare(a.name) )
     // fruits.sort((a,b) => b.calories - a.calories);
 
-    const lowCals = fruits.filter(fruit=> fruit.calories < 40)
-    const listItems = lowCals.map(fruit => <li key={fruit.id}>
+    // const lowCals = fruits.filter(fruit=> fruit.calories < 40)
+    // const highCallCals = fruits.filter(fruit=> fruit.calories >= 40)
+   
+     const itemList = props.items;
+
+    const listItems = itemList.map(fruit => <li key={fruit.id}>
                                                             {fruit.name} : &nbsp;
                                                            {fruit.calories}</li>)
     return (
