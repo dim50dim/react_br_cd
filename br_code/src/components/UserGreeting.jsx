748ef2e84482629abcf94f906cc1419760211cb2
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-19 08:30:13 +0200

    used ternary operator.

--- br_code/src/components/UserGreeting.jsx
+++ br_code/src/components/UserGreeting.jsx
@@ -1,5 +1,5 @@
 export default function UserGreeting(props) {
-      if(props.isLogged){
-        return <h2>Welcome {props.username} </h2>
-      }
+      return(props.isLogged ?
+                            <h2>Welcome {props.username} ternary </h2> :
+                            <h2>Please Log in for continue ternary</h2>)
 }
