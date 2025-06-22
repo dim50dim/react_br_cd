41db1d9308018c76ad1e90513cc709f0401cd399
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-22 13:34:09 +0200

    worked with input

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -1,29 +1,16 @@
 import { useState } from "react";
 
 export default function MyComponent() {
-    const [name,useName] = useState('Bob');
-    const [age,setAge] = useState(0)
-    const [isEmployed, setIsEmployed]  = useState(false);
-  function updateName(){
-    useName('Max')
-  }
-  const incrementAge  = () => setAge(age +1);
-  const toggleEmployedStatus = () => setIsEmployed(!isEmployed)
-
+       const [name,setName] = useState('Guest');
+       function handleChange (e) {
+        setName(e.target.value);
+       }
     return (
         <>
-        <div>
-            <p>Name : {name} </p>
-            <button onClick={updateName}>Set Name</button>
-        </div>
-        <div>
-            <p>Age: {age} </p>
-            <button onClick={incrementAge}>Set AGE</button>
-        </div>
-        <div>
-            <p> Employed: {isEmployed ? 'Yes' : 'NO'} </p>
-            <button onClick={toggleEmployedStatus}>Toggle Status</button>
-        </div>
+             <div>
+                <input value={name}  onChange={handleChange}/>
+                <p> Name: {name} </p>
+             </div>
         </>
     )
 }
