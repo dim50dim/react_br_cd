c5dd8e8f6b6416acf9f53fec58596893fc215f28
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-22 13:40:32 +0200

    worked with textarea.

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -2,14 +2,21 @@ import { useState } from "react";
 
 export default function MyComponent() {
        const [name,setName] = useState('Guest');
+       const [quantity,setQuantity] = useState(1)
        function handleChange (e) {
         setName(e.target.value);
+       }
+       function handleQuantity(e){
+        setQuantity(e.target.value)
        }
     return (
         <>
              <div>
                 <input value={name}  onChange={handleChange}/>
                 <p> Name: {name} </p>
+
+                <input value={quantity} onChange={handleQuantity} type="number" />
+                <p> Quantity : {quantity} </p>
              </div>
         </>
     )
