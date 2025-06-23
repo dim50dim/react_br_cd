34dbcf366f9b3492c60f6076db06ed7eafa0a30f
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-23 18:43:36 +0200

    created five functions.

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -2,7 +2,25 @@ import {  useState } from "react";
 
 export default function MyComponent() {
      const [cars,setCars] = useState([]);
+     const [carYear,setCarYear] = useState(new Date().getFullYear());
+     const [carMake, setCarMake] = useState('');
+     const [carModel, setCarModel] = useState('');
 
+     function handleAddCar(){
+      
+     }
+     function handleRemoveCar(index){
+
+     }
+     function handleYearChange(e){
+      
+     }
+     function handleMakeChange(e){
+
+     }
+     function handleModelChange(e){
+
+     }
     return (
       <>
 
