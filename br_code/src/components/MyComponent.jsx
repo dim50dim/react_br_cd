9858be767de6d5fa6cb3ea8d89bb72a4a7dc814c
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-23 16:26:24 +0200

    made function which remove item from array, through update state.

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -9,14 +9,14 @@ const [foods,setFoods] = useState(['Apple','Orange','Banana']);
         setFoods(f=>  [...f,newFood])
     }
     function handleRemoveFood(index){
-
+      setFoods(foods.filter((_,i) => i !== index))
     }
     return (
       <>
       <div>
         <h2>List of Food</h2>
         <ul>
-          {foods.map((food,index) => <li key={index}> {food} </li> )}
+          {foods.map((food,index) => <li key={index} onClick={() => handleRemoveFood(index)} > {food} </li> )}
         </ul>
         <input type="text" id="foodInput" placeholder="Enter food name"  />
         <button onClick={handleAddFood}> Add food </button>
