2c799711649aa06e0c72ef16884da423de80eaf0
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-23 18:52:55 +0200

    can not say it was easy.

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -7,23 +7,35 @@ export default function MyComponent() {
      const [carModel, setCarModel] = useState('');
 
      function handleAddCar(){
-      
+         const newCar = {year: carYear, make: carMake, model: carModel};
+         setCars(c => [...c,newCar]);
+         setCarYear(new Date().getFullYear())
      }
      function handleRemoveCar(index){
 
      }
      function handleYearChange(e){
-      
+      setCarMake(e.target.value)
      }
      function handleMakeChange(e){
-
+    setCarMake(e.target.value)
      }
      function handleModelChange(e){
-
+           setCarMake(e.target.value)
      }
     return (
       <>
-
+            <div>
+              <h2>List of Car Object</h2>
+              <ul>
+           {cars.map((elem,i) => 
+           <li key={i}> {elem.year}  {elem.make} {elem.model} </li> )}
+              </ul>
+              <input type="number" value={carYear}  onChange={handleYearChange}/><br />
+              <input type="text" value={carMake}  onChange={handleMakeChange} placeholder="enter car make"/><br />
+              <input type="text" value={carModel}  onChange={handleModelChange} placeholder="enter car model"/><br />
+              <button onClick={handleAddCar}> Add car</button>
+            </div>
       </>
     )
       
