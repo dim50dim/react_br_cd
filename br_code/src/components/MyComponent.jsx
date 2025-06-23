a3eaca2b296b5f73eb8ab7cb06270910d58aecdd
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-23 16:23:35 +0200

    created function which update state, for example, f => [...f,item] .

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -3,15 +3,24 @@ import {  useState } from "react";
 export default function MyComponent() {
 
 const [foods,setFoods] = useState(['Apple','Orange','Banana']);
-    function handleAddFood(e) {
-          setFoods()
+    function handleAddFood() {
+        const newFood = document.getElementById('foodInput').value;
+        document.getElementById('foodInput').value = '';
+        setFoods(f=>  [...f,newFood])
     }
-    function handleRemoveFood(e){
-      setFoods()
+    function handleRemoveFood(index){
+
     }
     return (
       <>
-      
+      <div>
+        <h2>List of Food</h2>
+        <ul>
+          {foods.map((food,index) => <li key={index}> {food} </li> )}
+        </ul>
+        <input type="text" id="foodInput" placeholder="Enter food name"  />
+        <button onClick={handleAddFood}> Add food </button>
+      </div>
       </>
     )
       
