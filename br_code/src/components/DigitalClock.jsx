2903700bcfcde9df2fbc895e663022a73bb6cd90
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-25 21:38:00 +0200

    created function formatTime().

--- br_code/src/components/DigitalClock.jsx
+++ br_code/src/components/DigitalClock.jsx
@@ -3,9 +3,23 @@ import { useEffect, useState } from "react"
 export default function DigitalClock() {
    const [time,setTime] = useState(new Date());
    useEffect (() => {
-    
+    const intervalId = setInterval(() => {
+        setTime(new Date())
+    }, 1000);
+
+    return () => {
+        clearInterval (intervalId);
+    }
    },[])
    
+   function formatTime() {
+    let hours = time.getHours()
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridiem = hours >= 12 ? "PM" : 'AM';
+
+    hours = hours % 12 
+   }
 
     return(
         <>
