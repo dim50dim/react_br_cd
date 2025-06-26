1dfeab660a493d2ad64d049805c49575bcfb47a2
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-26 20:14:28 +0200

    made Provider.

--- br_code/src/components/comp/ComponentA.jsx
+++ br_code/src/components/comp/ComponentA.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
 import ComponentB from "./ComponentB";
-
+export const UserContext = createContext();
 export default function ComponentA(){
     const [user,setUser] = useState('Bro');
+    
     return(
         <>
            <div className="box">
                    <h1>Component A</h1>
                    <h2> {`HELLO ${user}`} </h2>
+                   <UserContext.Provider value={user}>
                    <ComponentB user='Max'/>
+                   </UserContext.Provider>
            </div>
         </>
     )
