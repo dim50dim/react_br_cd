3574b044cabb3d11526b5d40985106b6e6c26d46
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-28 08:27:54 +0200

    could not say - it was easy.

--- br_code/src/components/StopWatch.jsx
+++ br_code/src/components/StopWatch.jsx
@@ -7,20 +7,29 @@ export default function StopWatch() {
     const startTimeRef = useRef(0);
 
     useEffect(() => {
-        
+        if(isRunning){
+       intervalIdRef.current =     setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current)
+            },10);
+        }
+        return () => {
+            clearInterval(intervalIdRef.current);
+        }
     },[isRunning])
      
     function start(){
-         
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
     }
     function stop(){
-
+        setIsRunning(false)
     }
     function reset(){
-
+      setIsRunning(0);
     }
     function formatTime() {
-             return `00:00:00`
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
+       
     }
     return (
         <>
