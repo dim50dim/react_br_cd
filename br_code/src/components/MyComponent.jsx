69600a349b4aae95c7a79ac5a45c9cc1fa898394
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-30 08:18:09 +0200

    added new state with new paragraph.

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -1,13 +1,19 @@
-import { useState } from "react"
+import {  useState } from "react"
 
 export default function MyComponent() {
     const [name, setName] = useState('Max');
+    const [age,setAge] = useState(0);
     const updateName =() => setName('John Doy');
+    const incrementAge =() => setAge(age + 1);
     return (
         <>
             <div>
                 <p> Name: {name} </p>
                 <button onClick={updateName} >Set Name</button>
+            </div>
+            <div>
+                <p> Age: {age} </p>
+                <button onClick={incrementAge} >Set Age</button>
             </div>
         </>
     )
