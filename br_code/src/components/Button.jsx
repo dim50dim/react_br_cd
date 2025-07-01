dc9a1377d01a7debbfb521a1a140622468eab072
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-01 11:15:29 +0200

    used event object - which is quite popular in programming.

--- br_code/src/components/Button.jsx
+++ br_code/src/components/Button.jsx
@@ -1,14 +1,15 @@
 export default function Button() {
    
   const handleClick = (e) => {
-
+     e.target.textContent = 'what is this happened'
+        
   }
 
   
  
     return(
         <>
-           <button onClick={() => handleClick('bro')} >Click us</button>
+           <button onClick={(e) => handleClick(e)} >Click us</button>
         </>
     )
 }
