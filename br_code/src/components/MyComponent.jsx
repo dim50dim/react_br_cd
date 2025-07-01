f5e1eee7f778a03d1c1bd4a677156dcb8fdefa9e
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-01 22:07:58 +0200

    deleted all content from App.js

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -1,10 +1,14 @@
-import {  useState } from "react"
+import {  useState,useEffect } from "react"
 
 export default function MyComponent() {
- 
+ const [count,setCount] = useState(0);
+ function addCount () {
+    setCount(c => c +1);
+ }
     return (
         <>
-   
+               <p> Count : {count}</p>
+               <button  onClick={addCount}> ADD </button>
         </>
     )
 }
