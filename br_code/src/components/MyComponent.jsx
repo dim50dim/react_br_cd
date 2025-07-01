e708c01fba0b77f507744634959e895eb63e2530
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-01 09:24:47 +0200

    made one more Button component.

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -1,12 +1,7 @@
 import {  useState } from "react"
 
 export default function MyComponent() {
-    const [name, setName] = useState('Max');
-    const [age,setAge] = useState(0);
-    const [isEmployed, setIsEmployed] = useState(false);
-    const isWorking = () => setIsEmployed(!isEmployed);
-    const updateName =() => setName('John Doy');
-    const incrementAge =() => setAge(age + 2);
+ 
     return (
         <>
    
