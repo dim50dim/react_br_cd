cde5d9cd948d27a294bdd94116cfae719a58b97e
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-01 09:34:59 +0200

    created function handleClick - which basically means, if you click less or equal 3 times , you will see {name} with the text you clicked me times.

--- br_code/src/components/Button.jsx
+++ br_code/src/components/Button.jsx
@@ -1,10 +1,21 @@
 export default function Button() {
-  const handleClick = () => console.log('WORKED');
-  
+    let count = 0;
+  const handleClick = (name) => {
+    if(count <3 ){
+        count++;
+        console.log(`${name} you clicked me ${count} time/s`);
+        
+    }else {
+        console.log(`${name} stop clicking me`);
+        
+    }
+  }
 
+  
+ 
     return(
         <>
-           <button onClick={handleClick} >Click us</button>
+           <button onClick={() => handleClick('bro')} >Click us</button>
         </>
     )
 }
