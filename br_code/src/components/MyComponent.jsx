5a0d8dd52211aa9a14a54f26a277b944eb1cef30
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-01 22:19:07 +0200

    made two different functions add and subtract number.

--- br_code/src/components/MyComponent.jsx
+++ br_code/src/components/MyComponent.jsx
@@ -2,13 +2,20 @@ import {  useState,useEffect } from "react"
 
 export default function MyComponent() {
  const [count,setCount] = useState(0);
+ useEffect(() => {
+     document.title = `Count : ${count}`
+ })
  function addCount () {
-    setCount(c => c +1);
- }
+    setCount(c => c +1)
+  }
+ function subtractCount () {
+    setCount(c =>c - 1)
+  }
     return (
         <>
                <p> Count : {count}</p>
                <button  onClick={addCount}> ADD </button>
+               <button  onClick={subtractCount}> Substract </button>
         </>
     )
 }
