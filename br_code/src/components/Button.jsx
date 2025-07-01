86ccc6ca6607d518e1b935701df4517255722353
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-01 11:17:53 +0200

    so, in general almost everything is clear, however, there are some misunderstanding.

--- br_code/src/components/Button.jsx
+++ br_code/src/components/Button.jsx
@@ -4,9 +4,7 @@ export default function Button() {
      e.target.textContent = 'what is this happened'
         
   }
-
   
- 
     return(
         <>
            <button onClick={(e) => handleClick(e)} >Click us</button>
