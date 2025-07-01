dcd20ee2465fb1bc24a354e887ea8549181cc67f
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-01 22:10:06 +0200

    have to remove all other files.

--- br_code/src/App.jsx
+++ br_code/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css';
-import React, { use } from 'react';
-import  { useState } from 'react';
-import ToDo from './components/ToDo';
+
+
 import MyComponent from './components/MyComponent';
-import DigitalClock from './components/DigitalClock';
-import ComponentA from './components/comp/ComponentA';
-import LastOne from './components/LastOne';
-import StopWatch from './components/StopWatch';
-import Button from './components/Button';
-import Button2 from './components/Button2';
 
 
 
 
+function App() {
+
+  return (
+    <>
+    <MyComponent/>
+    </>
+  )
+
+}
+export default App;
