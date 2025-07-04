a42800ee2aea700cc1f50d0ec9f67e6a8f55608a
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-04 09:25:33 +0200

    implemented useRef from react to start touching it.

--- br_code/src/components/DemoProps.jsx
+++ br_code/src/components/DemoProps.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function DemoProps() {
     const [count,setCount] = useState(0);
- 
+ const countRef = useRef(0);
     const increment =() => setCount(count + 1);
     const decrement =() => setCount(count - 1);
     return(
