7c9f6a53b66c2d17da9a2707a49568d0037925a9
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-04 09:40:10 +0200

    a bit messy.

--- br_code/src/components/DemoProps.jsx
+++ br_code/src/components/DemoProps.jsx
@@ -1,25 +1,21 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export default function DemoProps() {
-    const [count,setCount] = useState(0);
- const countRef = useRef(0);
-    const increment =() => {
-        setCount(count + 1);
-        countRef.current++;
+    const inputRef = useRef | null ;
 
-        console.log('State :', count);
-        console.log('Ref :' ,countRef.current);
-        
-        
-        
-    }
+    useEffect(() => {
+        inputRef.current?.focus();
+    },[]);
+    
+   
+    
     
     return(
         <>
            <div className="tutorial">
-            <h1> Count: {count} </h1>
         
-            <button onClick={increment} >Increment</button>
+        
+       <input type="text"  ref={inputRef} placeholder="Type something crucial"/>
            </div>
         </>
     )
