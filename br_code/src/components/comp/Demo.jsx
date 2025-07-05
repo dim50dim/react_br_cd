6f5f229b0005fb8a3e9bf949c4a0438e5151ce82
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-05 13:25:30 +0200

    easy thing.

--- br_code/src/components/comp/Demo.jsx
+++ br_code/src/components/comp/Demo.jsx
@@ -1,8 +1,16 @@
-export default function Demo() {
+import { useState } from "react"
 
+export default function Demo() {
+const [count,setCount] = useState(0);
+const handleClickPlus = () => setCount(count + 2);
+const handleClickMinus = () => setCount(count - 1);
     return(
         <>
+
            <h1>useStat Simply Explained!!!</h1>
+             <p>  Count : {count} </p>
+             <button onClick={handleClickPlus} > Plus me </button>
+             <button onClick={handleClickMinus} > Minus me </button>
         </>
     )
 }
