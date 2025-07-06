0165e212f76a8bee4f44369ef7603b41fa9b9e30
¨dim50dim¨ <zheliezkov@gmail.com>
2025-07-06 20:53:02 +0200

    right now , i am going to write new project here.

--- br_code/src/App.jsx
+++ br_code/src/App.jsx
@@ -1,18 +1,11 @@
 import './App.css';
-import Demo from './components/comp/Demo';
-import DemoProps from './components/DemoProps';
-
-
-import MyComponent from './components/MyComponent';
-
-
 
 
 function App() {
 
   return (
     <>
-        <Demo/>
+     
     </>
   )
 
